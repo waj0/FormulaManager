aa1e3ec5948f57f5f5ab8d43b6a1164f4abcca11
Majo Cecunda <Waj0@centrum.sk>
2014-04-10 22:18:49 +0200

    #1 main activity with list of formulas
    using fragments, in landscape layout displays also fragment to calculate
    formula

--- FormulaManager/app/src/main/java/cz/fi/android/formulamanager/app/MainActivity.java
+++ FormulaManager/app/src/main/java/cz/fi/android/formulamanager/app/MainActivity.java
@@ -1,11 +1,19 @@
 package cz.fi.android.formulamanager.app;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.support.v4.app.ListFragment;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Arrays;
+
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = "cz.fi.android.formulamanager.MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +21,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -27,11 +33,35 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    // method for searching formulas
+    @Override
+    public void onNewIntent(Intent intent) {
+        setIntent(intent);
+        Log.i(TAG, "search triggered");
+        handleIntent(intent);
+    }
+
+    // method for searching formulas
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.i(TAG, "search triggered "+ query);
+            doSearch(query);
+        }
+    }
+
+    private void doSearch(String queryStr) {
+        //TODO search in DB, may use content provider
+        FormulaAdapter adapter = new FormulaAdapter(this, Arrays.asList(FormulaListFragment.values).subList(1, 3));
+        ((ListFragment) getSupportFragmentManager().findFragmentById(R.id.formulas)).setListAdapter(adapter);
+        // get a Cursor, prepare the ListAdapter
+        // and set it
+    }
 }
